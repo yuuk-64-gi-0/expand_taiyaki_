3b37bcd1951ae9acfd81ff444f0145764d4975d4
yuu-kodama <yuuk_07_ya@yahoo.co.jp>
2024-01-17 12:49:49 +0900

    Update launcher.bat

--- script/launcher.bat
+++ script/launcher.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Do not close this console when you exit the process.
-echo Please close either the Control or Image window instead.
+echo Please close either the Controller or Image window instead.
 python3 volimg_ctrl.py
