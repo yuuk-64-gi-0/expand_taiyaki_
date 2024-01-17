a878040f698c05c56f0404763b7c0235a4c7c0e9
yuu-kodama <yuuk_07_ya@yahoo.co.jp>
2024-01-17 20:34:36 +0900

    Change the venv directory

--- Launch_in_venv.bat
+++ Launch_in_venv.bat
@@ -1,8 +1,8 @@
 @echo off
-if not exist .\venv\Scripts\activate (
+if not exist .\script\venv\Scripts\activate (
 echo "started to create virtual environment"
-python3 -m venv venv
-call .\venv\Scripts\activate
+python3 -m venv script\venv
+call .\script\venv\Scripts\activate
 echo "started to install required libraries"
 pip install -r .\script\requirements.txt --no-cache-dir
 call deactivate
@@ -12,7 +12,7 @@ rem
 )
 
 echo "activating virtual environment"
-call call .\venv\Scripts\activate.bat
+call call .\script\venv\Scripts\activate.bat
 cd script
 call launcher.bat
 call deactivate
